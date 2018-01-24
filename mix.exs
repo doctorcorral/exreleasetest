549d2e8378e4612dbcc25ba7db171c48d798da61
Ricardo C <ricardo@suggestic.com>
2018-01-24 15:37:34 -0600

    add changelog to ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule RepoExample.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6-dev",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [
+	main: "readme",
+	extras: ["README.md", "CHANGELOG.md"]
+      ]
     ]
   end
 
